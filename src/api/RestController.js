fa2efca5da92841a8d511ab1e0808a68f26e9edf
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-11-06 18:13:21 -0500

    RestController.js allocates all HTTP methods, LoginForm refactorized

---
+++ src/api/RestController.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+const apiUrl = "https://farrapp-api.herokuapp.com/";
+
+
+//USERS
+export function login(user, callback) {
+    axios.post(apiUrl + '/users/login', user)
+        .then(function (response) {
+            callback.onSuccess(response);
+        })
+        .catch(function (error) {
+            callback.onFailed(error);
+        })
+};
+
+export function addNewUser(user, callback) {
+    axios.post(apiUrl + '/users', user)
+        .then(function (response) {
+            callback.onSuccess(response);
+        })
+        .catch(function (error) {
+            callback.onFailed(error);
+        })
+};
+
+export function modifyUserInfo(email, user, callback) {
+  axios.put(apiUrl + '/users/user-info/' + email, user)
+      .then(function (response) {
+          callback.onSuccess(response);
+      })
+      .catch(function (error) {
+          callback.onFailed(error);
+      })
+};
+
+export function modifyUserPassword(email, user, callback) {
+  axios.put(apiUrl + '/users/user-password/' + email, user)
+      .then(function (response) {
+          callback.onSuccess(response);
+      })
+      .catch(function (error) {
+          callback.onFailed(error);
+      })
+};
+
+export function getUsers(callback) {
+  axios.get(apiUrl + '/users')
+      .then(function (response) {
+          callback.onSuccess(response);
+      })
+      .catch(function (error) {
+          callback.onFailed(error);
+      })
+};
+
+
+export function getUser(email, callback) {
+  axios.get(apiUrl + '/users/' + email)
+      .then(function (response) {
+          callback.onSuccess(response);
+      })
+      .catch(function (error) {
+          callback.onFailed(error);
+      })
+};
+
+
+//PARTIES
+
+

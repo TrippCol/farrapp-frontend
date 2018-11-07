2caff80023a5901eaddbdf3e738d63a64b2e069b
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-11-07 08:27:36 -0500

    Parties loaded, overlapping error on PartyScreen view

--- src/components/party/PartyScreen.js
+++ src/components/party/PartyScreen.js
@@ -9,67 +9,35 @@ import "../../css/app-party.css";
 import axios from "axios";
 
 class PartyScreen extends Component {
-  state = { focusParty: false, party: {} };
+  state = { focusParty: false, party: {} ,foundParties:{}};
+
+  componentDidMount(){
+    var self = this;
+    axios.get("https://farrapp-api.herokuapp.com/parties")
+    .then(function(response){
+      self.setState({foundParties: response.data});
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
 
   handleFocusParty = prty => {
     this.setState({ focusParty: true, party: prty , parties:null});
   };
 
-  renderSummary = async () => {
-    var self = this;
-    //try{
-      const response = await axios.get("https://farrapp-api.herokuapp.com/parties");
-      return Object.values(response.data).map(prt => {
-        return (
-          <PartySummary
-            key={prt.partyName}
-            party={prt}
-            focusHandler={self.handleFocusParty}
-          />
-        );
-      });
-    //}catch(error){
-      //console.error(error);
-    //}
-    //return undefined;
-    /*axios.get("https://farrapp-api.herokuapp.com/parties")
-      .then(function (response) {
-        var parties = response.data;
-        var data = (self.renderAll(parties));
-        self.setState({parties:data});
-      })
-      .catch(function (error) {
-        console.log(error);
-        data = undefined;
-      });*/
-  };
-
-  renderAll = (parties) => {
-    var self = this;
-    return Object.values(parties).map(prt => {
+  renderSummary = () => {
+    return Object.values(this.state.foundParties).map(prt => {
       return (
         <PartySummary
           key={prt.partyName}
           party={prt}
-          focusHandler={self.handleFocusParty}
+          focusHandler={this.handleFocusParty}
         />
       );
     });
   };
 
-  /*setParties = () => {
-    var callback = {
-      onSuccess: function(response){
-        return response.data;
-      },
-      onFailed: function(error){
-        console.log(error);
-        return undefined;
-      }
-    };
-
-    getPartyList(callback);
-  };*/
 
   renderParty = () => {
     return <Party party={this.state.party} />;
@@ -79,7 +47,6 @@ class PartyScreen extends Component {
     if (this.state.focusParty) {
       return <Layout className="container-party">{this.renderParty()}</Layout>;
     } else {
-      console.log(this.renderSummary());
       return (
         <Layout className="container-summary">
           <List
